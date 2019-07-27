cb442b4a1152b04590168022d0f59d0530163a80
LadyKerr <kadiekerr@gmail.com>
2019-07-27 18:42:49 -0400

    started components

--- e-makers/src/components/App.js
+++ e-makers/src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
+import Shops from "./Shops";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1> E-Makers App </h1>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <h1> E-Makers App </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">View Shops</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact="/shops" component={Shops} />
+      </div>
+    </Router>
   );
 }
 
